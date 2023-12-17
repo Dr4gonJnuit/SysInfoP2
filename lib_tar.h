aeff4fc3d220d7c6b37338bbbedd67adb9e8b97c
Jonas Bette <jonas.bette@student.uclouvain.be>
2023-12-17 10:48:41 +0100

    Micro changement

--- lib_tar.h
+++ lib_tar.h
@@ -6,38 +6,42 @@
 #include <stdint.h>
 #include <unistd.h>
 
-typedef struct posix_header
-{                              /* byte offset */
-    char name[100];               /*   0 */
-    char mode[8];                 /* 100 */
-    char uid[8];                  /* 108 */
-    char gid[8];                  /* 116 */
-    char size[12];                /* 124 */
-    char mtime[12];               /* 136 */
-    char chksum[8];               /* 148 */
-    char typeflag;                /* 156 */
-    char linkname[100];           /* 157 */
-    char magic[6];                /* 257 */
-    char version[2];              /* 263 */
-    char uname[32];               /* 265 */
-    char gname[32];               /* 297 */
-    char devmajor[8];             /* 329 */
-    char devminor[8];             /* 337 */
-    char prefix[155];             /* 345 */
-    char padding[12];             /* 500 */
-} tar_header_t;
+/* Placer nos propres includes ici */
+#include <string.h>
+#include <stdio.h>
 
-#define TMAGIC   "ustar"        /* ustar and a null */
-#define TMAGLEN  6              // Vaut 6 probablement parce que "ustar" a '\0' à la fin
-#define TVERSION "00"           /* 00 and no null */
+typedef struct posix_header
+{                       /* byte offset */
+    char name[100];     /*   0 */
+    char mode[8];       /* 100 */
+    char uid[8];        /* 108 */
+    char gid[8];        /* 116 */
+    char size[12];      /* 124 */
+    char mtime[12];     /* 136 */
+    char chksum[8];     /* 148 */
+    char typeflag;      /* 156 */
+    char linkname[100]; /* 157 */
+    char magic[6];      /* 257 */
+    char version[2];    /* 263 */
+    char uname[32];     /* 265 */
+    char gname[32];     /* 297 */
+    char devmajor[8];   /* 329 */
+    char devminor[8];   /* 337 */
+    char prefix[155];   /* 345 */
+    char padding[12];   /* 500 */
+} tar_header_t;         /* 512 */
+
+#define TMAGIC "ustar" /* ustar and a null */
+#define TMAGLEN 6      // Vaut 6 probablement parce que "ustar" a '\0' à la fin
+#define TVERSION "00"  /* 00 and no null */
 #define TVERSLEN 2
 
 /* Values used in typeflag field.  */
-#define REGTYPE  '0'            /* regular file */
-#define AREGTYPE '\0'           /* regular file */
-#define LNKTYPE  '1'            /* link */
-#define SYMTYPE  '2'            /* reserved */
-#define DIRTYPE  '5'            /* directory */
+#define REGTYPE '0'   /* regular file */
+#define AREGTYPE '\0' /* regular file */
+#define LNKTYPE '1'   /* link */
+#define SYMTYPE '2'   /* reserved */
+#define DIRTYPE '5'   /* directory */
 
 /* Converts an ASCII-encoded octal-based number into a regular integer */
 #define TAR_INT(char_ptr) strtol(char_ptr, NULL, 8)
@@ -102,7 +106,6 @@ int is_file(int tar_fd, char *path);
  *         any other value otherwise.
  */
 int is_symlink(int tar_fd, char *path);
-
 
 /**
  * Lists the entries at a given path in the archive.
